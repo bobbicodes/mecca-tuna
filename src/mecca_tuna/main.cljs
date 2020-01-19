04c2a1c9b1fb0e83fa0efb63e142f4b234c80d69
porkostomus <porkostomus@mail.com>
2020-01-19 09:17:54 -0800

    enter shadow-cljs

---
+++ src/mecca_tuna/main.cljs
@@ -0,0 +1,14 @@
+(ns mecca-tuna.main
+  (:require [reagent.core :as r]))
+
+(defn app []
+  [:div 
+   [:h1 "hi"]])
+
+(defn ^:dev/after-load start []
+  (r/render [app]
+            (.getElementById js/document "app")))
+
+(defn ^:export init
+  []
+  (start))
